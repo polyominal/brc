6f1bf7f0fb505b269d054e2eb57c5b4735064bb1
polyominal <79344013+polyominal@users.noreply.github.com>
2026-06-21 03:01:22 +0800

    map name slices into u64 keys

--- brc/src/main.rs
+++ brc/src/main.rs
@@ -1,9 +1,9 @@
 use memchr::memchr;
 
+type Key = u64;
+
 type Value = i32;
 type Sum = i64;
-
-const TEMPERATURE_RANGE_ABS: Value = 1000;
 
 fn slice_to_value(mut s: &[u8]) -> Value {
     let is_negative = if unsafe { *s.get_unchecked(0) } == b'-' {
@@ -23,38 +23,49 @@ fn slice_to_value(mut s: &[u8]) -> Value {
     if !is_negative { value } else { -value }
 }
 
-struct Record {
+struct Record<'a> {
+    key: &'a [u8],
     count: u32,
     min: Value,
     sum: Sum,
     max: Value,
 }
 
-impl Record {
+impl<'a> Record<'a> {
     fn add(&mut self, value: Value) {
         self.count += 1;
         self.min = self.min.min(value);
         self.sum += value as Sum;
         self.max = self.max.max(value);
     }
-}
 
-impl Default for Record {
-    fn default() -> Self {
+    fn new(key: &'a [u8], value: Value) -> Self {
         Self {
-            count: 0,
-            min: TEMPERATURE_RANGE_ABS,
-            sum: 0,
-            max: -TEMPERATURE_RANGE_ABS,
+            key,
+            count: 1,
+            min: value,
+            sum: value as Sum,
+            max: value,
         }
     }
+}
+
+// I assure you that this gives zero collision...
+fn slice_to_key(s: &[u8]) -> u64 {
+    let leading_dirty = unsafe { s.as_ptr().cast::<u64>().read_unaligned() };
+
+    // clean up garbage bytes. note that from_le should be a no-op for little-endian systems
+    let shift = 64_usize.saturating_sub(8 * s.len());
+    let leading = u64::from_le(leading_dirty) << shift;
+
+    leading ^ (s.len() as u64)
 }
 
 fn main() -> std::io::Result<()> {
     let data = std::fs::read("measurements.txt")?;
     let mut data = data.as_slice();
 
-    let mut map = std::collections::HashMap::<&[u8], Record>::new();
+    let mut map = std::collections::HashMap::<Key, Record>::new();
     while let Some(sep) = memchr(b';', data) {
         let value_len = memchr(b'\n', unsafe { data.get_unchecked(sep + 1..) }).unwrap();
         let (key, value) = unsafe {
@@ -64,19 +75,21 @@ fn main() -> std::io::Result<()> {
             )
         };
         let value = slice_to_value(value);
-        map.entry(key).or_default().add(value);
+        map.entry(slice_to_key(key))
+            .and_modify(|r| r.add(value))
+            .or_insert_with(|| Record::new(key, value));
         data = unsafe { data.get_unchecked(sep + 1 + value_len + 1..) };
     }
 
-    let mut list: Vec<_> = map.into_iter().collect();
-    list.sort_by_key(|p| p.0);
-    for (key, value) in list {
+    let mut list: Vec<_> = map.values().collect();
+    list.sort_by_key(|r| r.key);
+    for r in list {
         println!(
             "{key}: {min:.1}/{avg:.1}/{max:.1}",
-            key = unsafe { str::from_utf8_unchecked(key) },
-            min = value.min as f32 / 10_f32,
-            avg = (value.sum as f32 / value.count as f32) / 10_f32,
-            max = value.max as f32 / 10_f32
+            key = unsafe { str::from_utf8_unchecked(r.key) },
+            min = r.min as f32 / 10_f32,
+            avg = (r.sum as f32 / r.count as f32) / 10_f32,
+            max = r.max as f32 / 10_f32,
         );
     }
 
